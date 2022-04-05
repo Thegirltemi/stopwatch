6d338c538b60ab9e4c2da4b0bbe0bc8047408211
thegirltemi <temialamutu7@gmail.com>
2022-04-05 04:04:04 -0700

    first build

---
+++ script.js
@@ -0,0 +1,44 @@
+const seconds=document.querySelector('.seconds')
+const milliseconds=document.querySelector('.milliseconds')
+const start=document.querySelector('.start-btn')
+const reset=document.querySelector('.reset-btn')
+const stop=document.querySelector('.stop-btn')
+
+
+let sec=0;
+let millisec=0;
+var timeInterval;
+
+const timer=()=>{
+    millisec++;
+
+    if(millisec<9){
+        milliseconds.innerHTML="0"+millisec;
+    }
+    if(millisec>9){
+        milliseconds.innerHTML=millisec;
+    }
+    if(millisec>99){
+        sec++;
+        seconds.innerHTML="0"+sec;
+        millisec=0;
+        milliseconds.innerHTML="0"+0;
+    }
+    if(sec>9){
+        seconds.innerHTML=sec;
+    }
+}
+
+start.addEventListener('click',()=>{
+    timeInterval=setInterval(timer,10);   
+})
+stop.addEventListener('click',()=>{
+    clearInterval(timeInterval);
+})
+reset.addEventListener('click',()=>{
+  clearInterval(timeInterval);
+  sec="00"
+  millisec="00";
+  seconds.innerHTML=sec;
+  milliseconds.innerHTML=millisec;
+})
